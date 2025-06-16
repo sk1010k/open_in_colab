e95f862680ba7242f0b16daa66a2322725bd02e3
s_koyano <s_koyano@skoyano.local>
2025-06-17 00:11:54 +0900

    fix: add js files to load as extension

---
+++ parse.js
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+async function extractBranchName() {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (!tab.id)
+        return undefined;
+    const results = await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+            // GitHubのブランチ名を取得する例
+            const button = document.getElementById('branch-picker-repos-header-ref-selector');
+            const label = button?.getAttribute('aria-label');
+            const branchName = label?.split(' ')[0];
+            return branchName;
+        }
+    });
+    return results?.[0]?.result;
+}
+const COLAB_BASE_URL = 'https://colab.research.google.com/';
+const GITHUB_REPO_RE = /^https?:\/\/github\.com\/(.+)\/(.*\.ipynb)$/;
+const GITHUB_GIST_RE = /^https?:\/\/gist\.github\.com\/(.+)\/([a-f0-9]+(?:\#file\-.*\-ipynb)?)$/;
+/**
+ * Attempt to convert a GitHub notebook URL to a Colab URL.
+ *
+ * @param githubUrl The GitHub URL to convert.
+ *
+ * @return For valid GitHub URLs, a link to open the notebook in Colab,
+ * otherwise null.
+ */
+export async function githubToColabUrl(githubUrl) {
+    const repoMatch = GITHUB_REPO_RE.exec(githubUrl);
+    if (repoMatch) {
+        const branchName = await extractBranchName();
+        if (branchName) {
+            return COLAB_BASE_URL + ['github', repoMatch[1].replace(branchName, encodeURIComponent(branchName)), repoMatch[2]].join('/');
+        }
+        else {
+            return COLAB_BASE_URL + ['github', repoMatch[1], repoMatch[2]].join('/');
+        }
+    }
+    const gistMatch = GITHUB_GIST_RE.exec(githubUrl);
+    if (gistMatch) {
+        return COLAB_BASE_URL + ['gist', gistMatch[1], gistMatch[2]].join('/');
+    }
+    return null;
+}
---
+++ service_worker.js
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { githubToColabUrl } from './parse.js';
+// Details for calls to chrome.action.setPopup; see
+// https://developer.chrome.com/docs/extensions/reference/api/action#method-setPopup.
+const EMPTY_POPUP = {
+    popup: ''
+};
+const HELP_POPUP = {
+    popup: 'help.html'
+};
+// This listener is called when the user clicks the extension icon.
+//
+// If the current URL matches a Jupyter notebook hosted on github.com or on
+// gist.github.com, this function will open a new tab and load the notebook into
+// Colab.
+chrome.action.onClicked.addListener(async (tab) => {
+    if (!tab.url) {
+        console.warn('Open in Colab was invoked without a URL.');
+        return;
+    }
+    const colabUrl = await githubToColabUrl(tab.url);
+    if (!colabUrl) {
+        // Set and show a helpful popup page when the extension icon is clicked on
+        // an invalid URL, then unset it in case the next click is valid.
+        await chrome.action.setPopup(HELP_POPUP);
+        await chrome.action.openPopup();
+        await chrome.action.setPopup(EMPTY_POPUP);
+        console.warn(`Current page (${tab.url}) is not recognized as a GitHub-hosted notebook.`);
+        return;
+    }
+    await chrome.tabs.create({ 'url': colabUrl });
+});
